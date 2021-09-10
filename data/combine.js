80e03d9c5fc9a1148c0c4ee07c8929179547b641
Jonas Friedrich <pcjones@live.de>
2021-09-10 18:42:05 +0200

    Updates combine.js

--- data/combine.js
+++ data/combine.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
-const history1 = require("./history.json");
-const history2 = require("./historyHigh1.json");
+const history1 = require("./newHistory.json");
+const history2 = require("./history.json");
 //const history3 = require("./historyHi.json");
 // const history4 = require("./history20200309.json");
 // const history5 = require("./history20200520.json");
 
 const newHistory = history1.concat(history2)//.concat(history3).concat(history4)
 
-fs.writeFile(`./data/newHistory.json`, JSON.stringify(newHistory), function (err) {
+fs.writeFile(`./newHistory.json`, JSON.stringify(newHistory), function (err) {
     if (err) {
         console.log(err);
     }
