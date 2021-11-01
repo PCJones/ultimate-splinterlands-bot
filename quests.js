22adb7074447d8091d57f4873a89b004d45e5265
Nino <nma.tarzona2@gmail.com>
2021-11-02 03:13:57 +0800

    Update quests.js

--- quests.js
+++ quests.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = require("cross-fetch");
 
 const quests = [
     {name: "Defend the Borders", element: "life"},
@@ -19,26 +19,33 @@ const getQuestSplinter = (questName) => {
     return playerQuest.element;
 }
 
-const getPlayerQuest = (username) => (fetch(`https://game-api.splinterlands.io/players/quests?username=${username}`,
-  { "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
+const getPlayerQuest = async (username) => (await fetch(`https://game-api.splinterlands.io/players/quests?username=${username}`)//,
+  //{ "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
   .then(x => x && x.json())
   .then(x => {
       if (x[0]) {
           const questDetails = {name: x[0].name, splinter: getQuestSplinter(x[0].name), total: x[0].total_items, completed: x[0].completed_items}
           return questDetails;
         }})
-   .catch(() => {
-    console.log('Error: game-api.splinterlands did not respond trying api.slinterlands... ');
-    fetch(`https://api2.splinterlands.com/players/quests?username=${username}`,
-      { "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
+   .catch(async () => {
+    console.log('Error: game-api.splinterlands did not respond trying api2.slinterlands... ');
+    await fetch(`https://api2.splinterlands.com/players/quests?username=${username}`)
       .then(x => x && x.json())
       .then(x => {
           if (x[0]) {
               const questDetails = {name: x[0].name, splinter: getQuestSplinter(x[0].name), total: x[0].total_items, completed: x[0].completed_items}
               return questDetails;
             }})
-      .catch(e => console.log('[ERROR QUEST] Check if Splinterlands is down. Are you using username or email? please use username'))
+      .catch(async () => {
+    console.log('Error: api2.splinterlands did not respond trying api.slinterlands... ');
+    await fetch(`https://api.splinterlands.com/players/quests?username=${username}`)
+      .then(x => x && x.json())
+      .then(x => {
+          if (x[0]) {
+              const questDetails = {name: x[0].name, splinter: getQuestSplinter(x[0].name), total: x[0].total_items, completed: x[0].completed_items}
+              return questDetails;
+            }})
   })
-)
+}))
 
 module.exports.getPlayerQuest = getPlayerQuest;
