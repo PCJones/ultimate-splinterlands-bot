a21765506e62e84fbc95361b35a682a735005092
Nino <nma.tarzona2@gmail.com>
2021-11-02 03:20:35 +0800

    Update user.js

--- user.js
+++ user.js
@@ -1,17 +1,16 @@
-const fetch = require("node-fetch");
+const fetch = require("cross-fetch");
 const basicCards = require('./data/basicCards'); //phantom cards available for the players but not visible in the api endpoint
 
-getPlayerCards = (username, oneDayAgo) => (fetch(`https://game-api.splinterlands.io/cards/collection/${username}`,
-  { "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
+getPlayerCards = async (username, oneDayAgo) => (await fetch(`https://game-api.splinterlands.io/cards/collection/${username}`)//,
+  //{ "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
   .then(x => x && x.json())
   .then(x => x['cards'] ? x['cards'].filter(x=>(x.delegated_to === null || x.delegated_to === username)
   && (x.market_listing_type === null || x.delegated_to === username)
   && (!(x.last_used_player !== username && Date.parse(x.last_used_date) > oneDayAgo))).map(card => card.card_detail_id) : '')
   .then(advanced => basicCards.concat(advanced))
-   .catch(e=> {
+   .catch(async (e)=> {
     console.log('Error: game-api.splinterlands did not respond trying api.slinterlands... ');
-    fetch(`https://api.splinterlands.io/cards/collection/${username}`,
-      { "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
+    await fetch(`https://api.splinterlands.io/cards/collection/${username}`)
       .then(x => x && x.json())
       .then(x => x['cards'] ? x['cards'].filter(x=>(x.delegated_to === null || x.delegated_to === username) 
 	  && (x.market_listing_type === null || x.delegated_to === username)
