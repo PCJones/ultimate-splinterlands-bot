d5a7633fb4227d3d99ef319f8c5f3dc0840c016e
Nino <nma.tarzona2@gmail.com>
2021-11-04 06:09:01 +0800

    fix bug

--- quests.js
+++ quests.js
@@ -21,7 +21,7 @@ const getQuestSplinter = (questName) => {
 
 const getPlayerQuest = async (username) => (await fetch(`https://api.steemmonsters.io/players/quests?username=${username}`)//,
   //{ "credentials": "omit", "headers": { "accept": "application/json, text/javascript, */*; q=0.01" }, "referrer": `https://splinterlands.com/?p=collection&a=${username}`, "referrerPolicy": "no-referrer-when-downgrade", "body": null, "method": "GET", "mode": "cors" })
-  .then(async x => {if (!x.ok){throw new Error('Network response was not ok '+player);} x && x.json()})
+  .then(async x => {if (!x.ok){throw new Error('Network response was not ok ');} x && x.json()})
   .then(x => {
       if (x[0]) {
           const questDetails = {name: x[0].name, splinter: getQuestSplinter(x[0].name), total: x[0].total_items, completed: x[0].completed_items}
@@ -30,7 +30,7 @@ const getPlayerQuest = async (username) => (await fetch(`https://api.steemmonste
    .catch(async () => {
     console.log('Error: api.steemmonsters did not respond trying api2.slinterlands... ');
     await fetch(`https://api2.splinterlands.com/players/quests?username=${username}`)
-    .then(async x => {if (!x.ok){throw new Error('Network response was not ok '+player);} x && x.json()})
+    .then(async x => {if (!x.ok){throw new Error('Network response was not ok ');} x && x.json()})
       .then(x => {
           if (x[0]) {
               const questDetails = {name: x[0].name, splinter: getQuestSplinter(x[0].name), total: x[0].total_items, completed: x[0].completed_items}
@@ -39,7 +39,7 @@ const getPlayerQuest = async (username) => (await fetch(`https://api.steemmonste
       .catch(async () => {
     console.log('Error: api.splinterlands did not respond trying api2.slinterlands... ');
     await fetch(`https://game-api.splinterlands.io/players/quests?username=${username}`)
-    .then(async x => {if (!x.ok){throw new Error('Network response was not ok '+player);} x && x.json()})
+    .then(async x => {if (!x.ok){throw new Error('Network response was not ok ');} x && x.json()})
       .then(x => {
           if (x[0]) {
               const questDetails = {name: x[0].name, splinter: getQuestSplinter(x[0].name), total: x[0].total_items, completed: x[0].completed_items}
@@ -48,7 +48,7 @@ const getPlayerQuest = async (username) => (await fetch(`https://api.steemmonste
       .catch(async () => {
     console.log('Error: game-api.splinterlands did not respond trying api.steemmonsters... ');
     await fetch(`https://api.splinterlands.io/players/quests?username=${username}`)
-    .then(async x => {if (!x.ok){throw new Error('Network response was not ok '+player);} x && x.json()})
+    .then(async x => {if (!x.ok){throw new Error('Network response was not ok ');} x && x.json()})
       .then(x => {
           if (x[0]) {
               const questDetails = {name: x[0].name, splinter: getQuestSplinter(x[0].name), total: x[0].total_items, completed: x[0].completed_items}
