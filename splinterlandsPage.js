faaebea39bb949e4efc087934d9b2d29d98ffabb
Nino <nma.tarzona2@gmail.com>
2022-01-08 13:32:58 +0800

    fix can't login error and make login time instant.

--- splinterlandsPage.js
+++ splinterlandsPage.js
@@ -3,15 +3,14 @@ const misc = require('./misc');
 async function login(page) {
     try {
         page.waitForSelector('#log_in_button > button').then(() => page.click('#log_in_button > button'))
-        await page.waitForSelector('#email')
-            .then(() => page.waitForTimeout(3000))
+        await page.waitForSelector('#email',{visible: true, timeout: 10000})
             .then(() => page.focus('#email'))
             .then(() => page.type('#email', process.env.EMAIL))
             .then(() => page.focus('#password'))
             .then(() => page.type('#password', process.env.PASSWORD))
 
             // .then(() => page.waitForSelector('#login_dialog_v2 > div > div > div.modal-body > div > div > form > div > div.col-sm-offset-1 > button', { visible: true }).then(() => page.click('#login_dialog_v2 > div > div > div.modal-body > div > div > form > div > div.col-sm-offset-1 > button')))
-            .then(() => page.waitForSelector('#loginBtn',{visible: true, timeout: 1000})).then(button=>button.click())
+            .then(() => page.click('#loginBtn',{visible: true, timeout: 10000}))// .then(button=>button.click())
             //.then(() => page.waitForTimeout(1000))
             /*.then(() => page.reload())
             .then(() => page.waitForTimeout(5000))
